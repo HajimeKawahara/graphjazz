1a9b6e617a84d7738af3f3004237716f7b3b2d74
Hajime Kawahara <divrot@gmail.com>
2022-01-31 20:57:19 +0900

    ipynb examples added

--- src/graphjazz/plot/twcircle.py
+++ src/graphjazz/plot/twcircle.py
@@ -2,25 +2,28 @@ import matplotlib.pyplot as plt
 from matplotlib.patches import Circle
 import numpy as np
 
-def ntonic(ax,bytep,rcircle=0.17,rad=1.0,oncolor="black",offcolor="white",linecolor="black"):
+def ntonic(ax,bytep,rcircle=0.1,rad=1.0,oncolor="black",offcolor="white",linecolor="black"):
     """Plot N-tonic on a 12 circle
     
     Args:
        ax: axis
        bytep: bytep-value
-    
+       rcircle: small circle radius
+       rad: large circle radius
+       oncolor: on-color in small circles
+       offcolor: off-color in small circles
+       linecolor: line color of small circles
+
     """
     delta=np.pi/6.0
     for i,peach in enumerate(bytep):
         theta=delta*(i-1)+np.pi/2.0
         x=rad*np.cos(theta)
         y=rad*np.sin(theta)
-        print(x,y)
         if peach==1 or peach==True:
             c=Circle(xy=(x, y), radius=rcircle, facecolor=oncolor,edgecolor=linecolor)
         else:
-            c=Circle(xy=(x, y), radius=rcircle, facecolor=offcolor,edgecolor=linecolor)
-            
+            c=Circle(xy=(x, y), radius=rcircle, facecolor=offcolor,edgecolor=linecolor)            
         ax.add_patch(c)
 
 
---
+++ src/graphjazz/utils/__init__.py
@@ -0,0 +1,7 @@
+__version__ = "0.0.1"
+
+__uri__ = ""
+__author__ = "Hajime Kawahara"
+__email__ = "divrot@gmail.com"
+__license__ = ""
+__description__ = "graphjazz"
